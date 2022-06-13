efdab39f55c999a41dd051c5f0170712f6e28594
Gabriel Sardina <gabinex15@gmail.com>
2022-06-13 14:26:31 -0300

    [add] routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConversorDivisasComponent } from './divisas/pages/conversor-divisas/conversor-divisas.component';
+import { CrearLibroComponent } from './libros/pages/crear-libro/crear-libro.component';
+import { LibroComponent } from './libros/pages/libro/libro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LibroComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'divisas',
+    component: ConversorDivisasComponent,
+  },
+  {
+    path: 'nuevoLibro',
+    component: CrearLibroComponent,
+  },
+  // {
+  //   path: 'convertidor',
+  //   component: ConversorDivisasComponent,
+  // },
+  // {
+  //   path: 'generadorQr',
+  //   component: GeneradorQrComponent,
+  // },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
