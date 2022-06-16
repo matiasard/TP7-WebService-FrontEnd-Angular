0779335388a88bf0b150eb001d1b8f7de643a886
Gabriel Sardina <gabinex15@gmail.com>
2022-06-15 21:19:12 -0300

    [create] pasaje-form component

---
+++ src/app/pasaje/pages/pasaje-form/pasaje-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pasaje-form',
+  templateUrl: './pasaje-form.component.html',
+  styleUrls: ['./pasaje-form.component.css']
+})
+export class PasajeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
