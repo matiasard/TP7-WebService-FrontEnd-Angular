75ce4da440f0ec9841587bb0b7c26ec128459c19
Gabriel Sardina <gabinex15@gmail.com>
2022-06-17 17:53:11 -0300

    [add] pasaje-form component, [add] formulario reactivo

--- src/app/pasaje/pages/pasaje-form/pasaje-form.component.ts
+++ src/app/pasaje/pages/pasaje-form/pasaje-form.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PasajeService } from '../../services/pasaje.service';
 
 @Component({
   selector: 'app-pasaje-form',
   templateUrl: './pasaje-form.component.html',
-  styleUrls: ['./pasaje-form.component.css']
+  styleUrls: ['./pasaje-form.component.css'],
 })
 export class PasajeFormComponent implements OnInit {
+  //* Variables
+  categoriasPasaje: string[] = ['Menor', 'Adulto', 'Jubilado'];
+  pasajeros: any[] = [];
 
-  constructor() { }
+  //* ForGroup: Un formulario con un CONJUNTO fijo de CONTROLES que pueden administrar juntos.
+  //* Los controles de formulario individuales ahora se recopilan dentro de un grupo
+  pasajeFormulario = new FormGroup({
+    precioPasaje: new FormControl('', Validators.required),
+    categoriaPasajero: new FormControl('', Validators.required),
+    fechaCompra: new FormControl('', Validators.required),
+    pasajero: new FormControl('', Validators.required),
+  });
+
+  //* dependency injection
+  constructor(private pasajeService: PasajeService, private router: Router) {}
 
   ngOnInit(): void {
+    this.pasajeService.getAllPersonas().subscribe(
+      (personas) => {
+        this.pasajeros = personas.results;
+        console.log(this.pasajeros);
+      },
+      (error) => {
+        alert('Error al obtener el listado de personas');
+        console.warn(error);
+      }
+    );
   }
 
+  //* ************* Methods ****************
+
+  //* Crear nuevo pasaje
+  crearPasaje() {
+    console.log(this.pasajeFormulario.value);
+  }
 }
--- src/app/pasaje/pasaje.module.ts
+++ src/app/pasaje/pasaje.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PasajesComponent } from './pages/pasajes/pasajes.component';
 import { PasajeFormComponent } from './pages/pasaje-form/pasaje-form.component';
 
 @NgModule({
   declarations: [PasajesComponent, PasajeFormComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class PasajeModule {}
